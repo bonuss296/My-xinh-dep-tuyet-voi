fca9f599480a4b6e46e77bd304384f94a8a945b2
bonuss296 <89990956+bonuss296@users.noreply.github.com>
2021-09-02 22:11:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'hẹn em ngày hết dịch anh qua đón nhé công chúa.',
+    messDesc: 'Hẹn em ngày hết dịch anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
